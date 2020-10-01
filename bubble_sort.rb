a2dd7e00119f14c9172e4e47f37ad388b615de38
Gzim Asani <gzimasanni@gmail.com>
2020-10-01 10:30:38 +0200

    fix the linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,7 +4,7 @@ def bubble_sort(array)
     swapped = false
 
     (n - 1).times do |i|
-      if array[i] > array[i + 1] next
+      if array[i] > array[i + 1] 
         array[i], array[i + 1] = array[i + 1], array [i]
         swapped = true
 
