66799ea51df0cc3555361954ed862ab11ee00d09
Gzim Asani <gzimasanni@gmail.com>
2020-10-01 10:38:30 +0200

    Fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,11 +4,10 @@ def bubble_sort(array)
     swapped = false
 
     (n - 1).times do |i|
-      if array[i] > array[i + 1] 
-        array[i], array[i + 1] = array[i + 1], array [i]
-        swapped = true
+      next unless array[i] > array[i + 1]
 
-      end
+      array[i], array[i + 1] = array[i + 1], array [i]
+      swapped = true
     end
     break unless swapped
   end
@@ -18,34 +17,17 @@ end
 a = [9, 2, 14, 11, 101, 99, 87, 200, 128, 32, 12, 87]
 p bubble_sort(a)
 
-def bubble_sort_by(arr)
-  count = arr.length
-  while count > 0 do
-    n = 0
-    while n < arr.length-1 do
-      if block_given?
-        result = yield(arr[n], arr[n+1])
-        if result == -1 
-          arr[n+1], arr[n] = arr[n], arr[n+1]
-          count += arr.length 
-        else
-          count -= 1
-        end
-        n += 1 
-      else 
-        result = arr[n] > arr[n+1]
-        if result 
-          arr[n+1], arr[n] = arr[n], arr[n+1]
-          count += arr.length 
-        else
-          count -= 1
-        end
-        n += 1  
-      end
-    end  
+def bubble_sort_by(array2)
+  (array2.length - 1).times do |i|
+    array2[i], array2[i + 1] = array2[i + 1], array2[i] if yield(array2[i], array2[i + 1]).positive?
   end
-  print arr
+  array2
 end
 
-bubble_sort_by([55,75,345,56,7,8,3,4,7,8,2,9,4,12,3,5,9,3,1,5,676,34,23,8,12]) { |left,right| left <=> right}
-bubble_sort_by(["hi","hello","hey"]) { |left,right| right <=> left}
+new_array = bubble_sort_by(%w[hi hello hey ashjbas]) do |left, right|
+  left.length - right.length
+end
+
+puts new_array
+
+bubble_sort_by(%w[hi hello hey]) { |left, right| right <=> left }
